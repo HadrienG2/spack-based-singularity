9f386dc4e80475ad543feed6621717c3671dbbe4
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-26 14:54:55 +0100

    Terminology fix

--- rebuild-singularity-spack.sh
+++ rebuild-singularity-spack.sh
@@ -9,10 +9,14 @@ echo "*** Deleting old Singularity images ***"
 rm -f ${IMAGES_ROOT}/tumbleweed.sif
 rm -f ${IMAGES_ROOT}/spack.sif
 
-echo "*** Generating base Tumbleweed image ***"
+echo "*** Building base Tumbleweed image ***"
 cd tumbleweed
 singularity build ${IMAGES_ROOT}/tumbleweed.sif tumbleweed.def
 
-echo "*** Generating Spack image ***"
+echo "*** Building Spack image ***"
 cd ../spack
 singularity build ${IMAGES_ROOT}/spack.sif spack.def
+
+echo "*** Building Verrou image ***"
+cd ../verrou
+singularity build ${IMAGES_ROOT}/verrou.sif verrou.def
