0133eb45f9625f764ea0859f40887ad808f5ac5c
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-26 13:22:42 +0100

    It seems Singularity does not like to overwrite, so let's pre-delete

--- rebuild-singularity-spack.sh
+++ rebuild-singularity-spack.sh
@@ -5,6 +5,10 @@ IFS=$'\n\t'
 # Build parameters
 IMAGES_ROOT="/home/hadrien/Software/singularity-images/"
 
+echo "*** Deleting old Singularity images ***"
+rm -f ${IMAGES_ROOT}/tumbleweed.sif
+rm -f ${IMAGES_ROOT}/spack.sif
+
 echo "*** Generating base Tumbleweed image ***"
 cd tumbleweed
 singularity build ${IMAGES_ROOT}/tumbleweed.sif tumbleweed.def
