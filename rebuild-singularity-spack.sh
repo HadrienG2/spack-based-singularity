eb1ba7c963eedc1052f471838441cfe112362904
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-27 10:19:28 +0100

    Turns out there _is_ a force option :)

--- rebuild-singularity-spack.sh
+++ rebuild-singularity-spack.sh
@@ -8,24 +8,18 @@ IMAGES_ROOT="/home/hadrien/Software/singularity-images"
 VERROU_VERSION="2.1.0"
 ROOT_VERSION="6.16.00"
 
-echo "*** Deleting old Singularity images ***"
-rm -f ${IMAGES_ROOT}/tumbleweed.sif
-rm -f ${IMAGES_ROOT}/spack.sif
-rm -f ${IMAGES_ROOT}/verrou-${VERROU_VERSION}.sif
-rm -f ${IMAGES_ROOT}/root-${ROOT_VERSION}-cxx17.sif
-
 echo "*** Building base Tumbleweed image ***"
 cd tumbleweed
-singularity build ${IMAGES_ROOT}/tumbleweed.sif tumbleweed.def
+singularity build --force ${IMAGES_ROOT}/tumbleweed.sif tumbleweed.def
 
 echo "*** Building Spack image ***"
 cd ../spack
-singularity build ${IMAGES_ROOT}/spack.sif spack.def
+singularity build --force ${IMAGES_ROOT}/spack.sif spack.def
 
 echo "*** Building Verrou image ***"
 cd ../verrou
-singularity build ${IMAGES_ROOT}/verrou-${VERROU_VERSION}.sif verrou.def
+singularity build --force ${IMAGES_ROOT}/verrou-${VERROU_VERSION}.sif verrou.def
 
 echo "*** Building ROOT image ***"
 cd ../root
-singularity build ${IMAGES_ROOT}/root-${ROOT_VERSION}-cxx17.sif root.def
+singularity build --force ${IMAGES_ROOT}/root-${ROOT_VERSION}-cxx17.sif root.def
