289979002f3d647905f30d7de0419b711ccff5e2
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-02-27 10:34:00 +0100

    Add Gaudi image to the build script

--- rebuild-singularity-spack.sh
+++ rebuild-singularity-spack.sh
@@ -23,3 +23,7 @@ singularity build --force ${IMAGES_ROOT}/verrou-${VERROU_VERSION}.sif verrou.def
 echo "*** Building ROOT image ***"
 cd ../root
 singularity build --force ${IMAGES_ROOT}/root-${ROOT_VERSION}-cxx17.sif root.def
+
+echo "*** Building Gaudi image ***"
+cd ../gaudi
+singularity build --force ${IMAGES_ROOT}/gaudi.sif gaudi.def
